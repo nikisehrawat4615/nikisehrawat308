566d640fea596514bb17436e22ba848456df6591
reshmaSehrawat <nikisehrawat989@gmail.com>
2024-12-20 23:01:07 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  important: true,
-  darkMode: 'class',
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('tailwindcss-primeui')],
-  prefix: 'tw-'
-}
-
+   important: true,
+   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+   theme: {
+      extend: {},
+   },
+   plugins: [],
+   prefix: "tw-",
+};
--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          // treat all tags with a dash as custom elements
-          isCustomElement: (tag) => tag.includes('qlik-')
-        }
-      }
-    })
+    vue(),
   ],
   resolve: {
     alias: {

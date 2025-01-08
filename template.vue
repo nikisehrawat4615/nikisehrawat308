37f16196f9ebac0a9a07a2684b1fde86343c8bf3
reshmaSehrawat <nikisehrawat989@gmail.com>
2025-01-08 13:18:51 +0530

    Add files via upload

--- template.vue
+++ template.vue
@@ -1,11 +1,7 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-
+   
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue({
-//       template: {
-//         compilerOptions: {
-//           // treat all tags with a dash as custom elements
-//           isCustomElement: (tag) => tag.includes('qlik-')
-//         }
-//       }
-//     })
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   }
-// })
-
-// vite.config.js
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
-
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'), // This sets the '@' alias to the 'src' folder
-    },
-  },
-});
-
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
